a97db006235dfb4de685b76f557936a5a77ed99d
Bojie Li <bojieli@gmail.com>
2013-06-04 14:19:07 +0800

    add single/batch mode argument in rx

--- rx/main.c
+++ rx/main.c
@@ -15,12 +15,42 @@
 static pthread_mutex_t irq_lock;
 struct timeval begin;
 int sockfd; // socket to master
+int batch_count = 0;
 
-// received data from nRF24l01
-void on_irq(void)
+static void print_buf(uchar* buf)
+{
+    int i;
+    for (i=0; i<BUF_SIZE; i++)
+        printf("%02x ", buf[i]);
+}
+
+static void batch(uchar* buf)
 {
     static int total = 0, wrong = 0;
     static int last = 0;
+    static int last_time = 0;
+    ++total;
+    if (buf[BUF_SIZE-1] != 0 && buf[BUF_SIZE-1] != last + 1) {
+        ++wrong;
+    }
+    last = buf[BUF_SIZE-1];
+
+    if (total % batch_count == 0) {
+        struct timeval now;
+        gettimeofday(&now, NULL);
+        int total_time = (now.tv_usec - begin.tv_usec) / 1000 + (now.tv_sec - begin.tv_sec) * 1000;
+        print_buf(buf);
+        printf("time %6d diff %6d rate %7d ", total_time, total_time - last_time, (int)(total*1000000.0/total_time));
+        last_time = total_time;
+        printf("total %8d wrong %8d rate %lf", total, wrong, wrong * 1.0 / total);
+        printf("\n");
+        fflush(stdout);
+    }
+}
+
+// received data from nRF24l01
+static void on_irq(void)
+{
     uchar buf[BUF_SIZE];
 
     if (0 != pthread_mutex_trylock(&irq_lock))
@@ -33,23 +63,11 @@ void on_irq(void)
             printf("socket error\n");
         }
 
-        ++total;
-        if (buf[BUF_SIZE-1] != 0 && buf[BUF_SIZE-1] != last + 1) {
-            ++wrong;
-        }
-        last = buf[BUF_SIZE-1];
-
-        if (total % 1000 == 0) {
-            struct timeval now;
-            gettimeofday(&now, NULL);
-            int total_time = (now.tv_usec - begin.tv_usec) / 1000 + (now.tv_sec - begin.tv_sec) * 1000;
-            int i;
-            for (i=0; i<BUF_SIZE; i++)
-                printf("%02x ", buf[i]);
-            printf("time %6d rate %7d ", total_time, (int)(total*1000000.0/total_time));
-            printf("total %d\t wrong %d\t rate %lf\t", total, wrong, wrong * 1.0 / total);
+        if (batch_count > 0)
+            batch(buf);
+        else {
+            print_buf(buf);
             printf("\n");
-            fflush(stdout);
         }
     } else {
         printf("Receive failed on IRQ\n");
@@ -110,11 +128,16 @@ int main(int argc, char** argv)
     }
 
     int station = 0;
-    if (argc == 2)
+    if (argc >= 2)
         station = atoi(argv[1]);
     init_NRF24L01(station & 0x7F); // maximum 127 channels
 
-    printf("Initialized channel %d.\n", station);
+    if (argc == 3)
+        batch_count = atoi(argv[2]);
+    else
+        batch_count = 0;
+
+    printf("Initialized channel %d in %s mode.\n", station, batch_count ? "batch" : "single");
     pthread_mutex_lock(&irq_lock);
     print_configs();
     pthread_mutex_unlock(&irq_lock);
