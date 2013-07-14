77171ccb8125320c01091d8143ead892b2d980c8
Bojie Li <bojieli@gmail.com>
2013-07-14 20:52:44 +0800

    use pthread_join, remove ugly thread locks

--- merger/merger.c
+++ merger/merger.c
@@ -1,26 +1,9 @@
 #include "common.h"
 
-pthread_mutex_t lock_threads_running;
 pthread_mutex_t lock_notify_queue;
 pthread_mutex_t lock_timers;
 
 dict students, timers;
-
-static void* init_server_helper(void* arg) {
-    init_server();
-    pthread_mutex_unlock(&lock_threads_running);
-    return NULL;
-}
-static void* init_timeout_helper(void* arg) {
-    init_timeout();
-    pthread_mutex_unlock(&lock_threads_running);
-    return NULL;
-}
-static void* init_sender_helper(void* arg) {
-    init_sender();
-    pthread_mutex_unlock(&lock_threads_running);
-    return NULL;
-}
 
 int main() {
     if (!load_config()) {
@@ -30,20 +13,16 @@ int main() {
 
     pthread_mutex_init(&lock_notify_queue, NULL);
     pthread_mutex_init(&lock_timers, NULL);
-    pthread_mutex_init(&lock_threads_running, NULL);
-    pthread_mutex_lock(&lock_threads_running);
 
     students = new_dict();
     timers = new_dict();
 
-    pthread_t tid_server, tid_sender;
-    pthread_create(&tid_server, NULL, &init_server_helper, NULL);
-    pthread_create(&tid_server, NULL, &init_timeout_helper, NULL);
-    pthread_create(&tid_sender, NULL, &init_sender_helper, NULL);
+    pthread_t tid_server, tid_timeout, tid_sender;
+    pthread_create(&tid_server,  NULL, (void * (*)(void *))&init_server,  NULL);
+    pthread_create(&tid_timeout, NULL, (void * (*)(void *))&init_timeout, NULL);
+    pthread_create(&tid_sender,  NULL, (void * (*)(void *))&init_sender,  NULL);
 
-    // wait until any of the threads terminate
-    // In fact, only server thread may exit
-    pthread_mutex_lock(&lock_threads_running);
+    pthread_join(tid_server, NULL);
     debug("Program terminated\n");
     return 0;
 }
