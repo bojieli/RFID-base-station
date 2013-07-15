ff6dc2e03ac9d0f9e14b9abdf21c5d2449a9fd0e
Bojie Li <bojieli@gmail.com>
2013-07-15 22:17:01 +0800

    deploy.sh fix pwd

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,6 +3,10 @@ if [ `whoami` != 'root' ]; then
     echo "You must be root!"
     exit 1
 fi
+if [ `dirname $0` != '.' ]; then
+    echo "You must run the script in its directory!"
+    exit 1
+fi
 
 mv /etc/rc.local /etc/rc.local.old
-ln -s `dirname $0`/rc.local /etc/rc.local
+ln -s `pwd`/rc.local /etc/rc.local
