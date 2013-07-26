1c2a5fcf12a290b0f2ee86ce1d7af14ea670a442
Bojie Li <bojieli@gmail.com>
2013-07-27 02:09:40 +0800

    fix returning local variable on stack

--- common/common.c
+++ common/common.c
@@ -1,13 +1,16 @@
 #include "common.h"
 
+#define TIME_BUFSIZE 25
+static char time_strbuf[TIME_BUFSIZE] = {0};
+
+// WARNING: This function is neither reentrant nor thread-safe.
 char* print_time() {
     time_t timer;
-    char buffer[25];
     struct tm* tm_info;
 
     time(&timer);
     tm_info = localtime(&timer);
 
-    strftime(buffer, 25, "%Y-%m-%d %H:%M:%S", tm_info);
-    return buffer;
+    strftime(time_strbuf, TIME_BUFSIZE, "%Y-%m-%d %H:%M:%S", tm_info);
+    return time_strbuf;
 }
