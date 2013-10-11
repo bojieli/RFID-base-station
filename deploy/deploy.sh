3107701bab59c4c31405e9f53d0bd2a702de1f5d
Bojie Li <bojieli@gmail.com>
2013-10-11 18:03:10 +0800

    output vendor while installing

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -62,6 +62,7 @@ if [ "$ACTION" == "install" ]; then
     echo "master ip: $MASTER_IP"
     echo "slave ip: $SLAVE_IP"
     echo "access token: $ACCESS_TOKEN"
+    echo "vendor: $VENDOR"
 fi
 
 if [ `whoami` != 'root' ]; then
