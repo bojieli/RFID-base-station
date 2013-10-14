5849688d012170924176aaedc6ccb403d61d2c14
Bojie Li <bojieli@gmail.com>
2013-10-14 13:51:04 +0800

    retrieve master IP dynamically from the server

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -12,7 +12,7 @@ isip()
 }
 
 if [ -z "$1" ]; then
-    echo "Usage: ./deploy.sh [ master <ip-of-slave> <access-token> | slave <ip-of-master> <access-token> | update ] [<vendor>]"
+    echo "Usage: ./deploy.sh [ master <access-token> | slave <access-token> | update ] [<vendor>]"
     exit 1
 elif [ "$1" == "update" ]; then
     ACTION=update
@@ -23,35 +23,23 @@ elif [ "$1" == "update" ]; then
     fi
     VENDOR=${2:-default}
 elif [ "$1" == "master" ]; then
-    if ! isip "$2"; then
-        echo "slave IP is invalid"
-        exit 1
-    fi
-    if [ -z "$3" ]; then
+    if [ -z "$2" ]; then
         echo "Please specify access token"
         exit 1
     fi
     ACTION=install
     TARGET=$1
-    MASTER_IP="127.0.0.1"
-    SLAVE_IP=$2
-    ACCESS_TOKEN=$3
-    VENDOR=${4:-default}
+    ACCESS_TOKEN=$2
+    VENDOR=${3:-default}
 elif [ "$1" == "slave" ]; then
-    if ! isip "$2"; then
-        echo "master IP is invalid"
-        exit 1
-    fi
-    if [ -z "$3" ]; then
+    if [ -z "$2" ]; then
         echo "Please specify access token"
         exit 1
     fi
     ACTION=install
     TARGET=$1
-    MASTER_IP=$2
-    SLAVE_IP="127.0.0.1"
-    ACCESS_TOKEN=$3
-    VENDOR=${4:-default}
+    ACCESS_TOKEN=$2
+    VENDOR=${3:-default}
 else
     echo "Please specify master, slave or update as first parameter"
     exit 1
@@ -59,8 +47,6 @@ fi
 
 echo "$ACTION for $TARGET."
 if [ "$ACTION" == "install" ]; then
-    echo "master ip: $MASTER_IP"
-    echo "slave ip: $SLAVE_IP"
     echo "access token: $ACCESS_TOKEN"
     echo "vendor: $VENDOR"
 fi
@@ -88,10 +74,8 @@ if [ "$ACTION" == "install" ]; then
         cp -a $CODE_BASE/config/* $INSTALL_DIR/etc/
     fi
     MERGER_CONF=$INSTALL_DIR/etc/merger.ini
-    sed -i "s/^listen.local_ip = .*$/listen.local_ip = $MASTER_IP/" $MERGER_CONF
     sed -i "s/^cloud.access_token = .*$/cloud.access_token = $ACCESS_TOKEN/" $MERGER_CONF
     RECEIVER_CONF=$INSTALL_DIR/etc/receiver.ini
-    sed -i "s/^master.ip = .*$/master.ip = $MASTER_IP/" $RECEIVER_CONF
     sed -i "s/^cloud.access_token = .*$/cloud.access_token = $ACCESS_TOKEN/" $RECEIVER_CONF
 fi
 
@@ -135,10 +119,7 @@ if [ "$ACTION" == "install" ]; then
     echo $TARGET > /etc/hostname
     hostname $TARGET
     sed -i '/^127\.0\.0\.1.*/d' /etc/hosts
-    sed -i '/\(master\|slave\)$/d' /etc/hosts
     echo "127.0.0.1 localhost" >> /etc/hosts
-    echo "$MASTER_IP master" >> /etc/hosts
-    echo "$SLAVE_IP slave" >> /etc/hosts
 fi
 
 # start services
